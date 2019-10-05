1fc97959fcc39fe3ca5e971a909b85f32e7a4db9
Marvin <marvin.polette@gmail.com>
2019-10-05 19:11:23 +0200

    add component eat and stats prey

---
+++ Assets/Scripts/Player/BlobEat.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobEat : MonoBehaviour
+{
+    private Dictionary<string, int> _preys = new Dictionary<string, int>();
+    public Dictionary<string, int> Preys
+    {
+        get { return _preys; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(IsEdible(collision.transform.tag))
+        {
+            Eat(collision.transform);
+        }
+    }
+
+    private void Eat(Transform prey)
+    {
+        SetPrey(prey.tag);
+        Destroy(prey.gameObject);
+    }
+
+    private bool IsEdible(string prey)
+    {
+        if(prey == "Prey") return true;
+        return false;
+    }
+
+    private void SetPrey(string prey)
+    {
+        if(_preys.ContainsKey(prey))
+        {
+            foreach(KeyValuePair<string, int> currentPrey in _preys)
+            {
+                if(currentPrey.Key == prey)
+                {
+                    _preys[currentPrey.Key] += 1;
+                    return;
+                }
+            }
+        } 
+        else
+        {
+            _preys.Add(prey, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Prey/PreyStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreyStats : MonoBehaviour
+{
+    [SerializeField]
+    private string _name = "test";
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    [SerializeField]
+    private Vector3 _mass = new Vector3(1, 1, 0);
+    public Vector3 Mass
+    {
+        get { return _mass; }
+    }
+}
