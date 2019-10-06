40db56d3109a96267782724fe47aa0f45283333f
corrado <corradolongoni@yahoo.com>
2019-10-06 15:07:42 +0200

    platform destroy

---
+++ Assets/Scripts/Player/Movement/Testers/PlatformDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour
+{
+    private PlayerMovementGroundSticky playerMovementGroundSticky;
+    [SerializeField] PooledBullet prefab;
+
+    void Start()
+    {
+        playerMovementGroundSticky = transform.parent.gameObject.GetComponent<PlayerMovementGroundSticky>();
+    }
+
+    void DestroyObject(GameObject go)
+    {
+        prefab.Get<PooledBullet>();
+        prefab.transform.position = go.transform.position;
+        prefab.transform.localScale = Vector3.one * playerMovementGroundSticky.SizeMul;
+        Destroy(go); 
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.bounds.size.x < playerMovementGroundSticky.SizeMul) DestroyObject(other.gameObject);
+        if (other.bounds.size.y < playerMovementGroundSticky.SizeMul) DestroyObject(other.gameObject);
+    }
+}
